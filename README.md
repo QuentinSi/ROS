8f9b5d58f50403ba9f6f8f79b4ef306cc3b29765
Quentin <quentin.simon@ensea.fr>
2022-07-19 13:48:53 +0200

    update readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Project ROS for WidowX XM430
+![github-large](https://raw.githubusercontent.com/Interbotix/interbotix_ros_turrets/main/interbotix_ros_xsturrets/images/xsturret_banner.png)
+
+This project is for the WidowX Xm430 robot. The WidowX XM430 Robot Turret is a fully programmable pan and tilt system designed around the DYNAMIXEL servos from Robotis.The servos offer a high resolution of 4096 positions and user a definable PID parameters. Temperature monitoring, positionnal feedback as well as voltage levels, load and compliance settings are all user accessible as well.
+
+Let's look at its performance graph:
+
+![github-large](https://emanual.robotis.com/assets/images/dxl/x/xm430_w350_performance_graph.jpg)
+
+The pckg folder is the package where we will find the source files, messages, CMakeLists and package.xml.
+
+This project was used to be able to have control over this turret. This is why you will find all the necessary information in this package to make this project work, continue it, test it or improve it.
--- pckg/src/pan_tilt.cpp
+++ pckg/src/pan_tilt.cpp
@@ -7,9 +7,6 @@
 #include "pckg/JointGroupCommand.h"
 #include "pckg/JointTrajectoryCommand.h"
 #include <ros/time.h>
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 
 //This is the class with the constructor and the methods for only the simulation Gazebo
 class pan_tilt_simu{
@@ -37,7 +34,6 @@ public:
     }
     //function to display the data of the robot you want
     void get_position_arm(const sensor_msgs::JointState& state){
-        //ROS_INFO("RECEIVED JOINT VALUES");
         if (state.name.size()== 2 ) {
            for (int i=0;i<2;i++){
             ROS_INFO_STREAM("\n RECEIVED JOINT VALUES :"
@@ -51,15 +47,16 @@ public:
     }
     //function to set max and min values ​​for gazebo simulation
     void set_limits(){
-        if (angle_pan.data<-3.14) angle_pan.data=-3.14;
-        else if (angle_pan.data>3.14) angle_pan.data=3.14;
-        else if (angle_tilt.data<-1.57) angle_tilt.data=-1.57;
-        else if (angle_tilt.data>1.57) angle_tilt.data=1.57;
+        if (angle_pan.data<-3.14) angle_pan.data=-3.139;
+        else if (angle_pan.data>3.14) angle_pan.data=3.139;
+        else if (angle_tilt.data<-1.57) angle_tilt.data=-1.569;
+        else if (angle_tilt.data>1.57) angle_tilt.data=1.569;
     }
     //function that allows to put the robot of the gazebo simulation in initial position
     void init_angle(){
-        angle_pan.data= 0.0;
-        angle_tilt.data= 0.0;
+        angle_pan.data= -5.0;
+        angle_tilt.data= -2.0;
+        set_limits();
         arm_pub_pan.publish(angle_pan);
         arm_pub_tilt.publish(angle_tilt);
     }
@@ -74,7 +71,7 @@ public:
     //function which makes it possible to vary the position of the robot from its smallest value to its largest value (pan motor)
     void move_auto_pan(){
             arm_pub_pan.publish(angle_pan);
-            if(angle_pan.data>=3.14)  pan_sim =1;
+            if(angle_pan.data>=3.139)  pan_sim =1;
             if(angle_pan.data<=-3.139) pan_sim=0;
 
             if(pan_sim==1) angle_pan.data=angle_pan.data-0.2;
@@ -83,8 +80,8 @@ public:
     //function which makes it possible to vary the position of the robot from its smallest value to its largest value (tilt motor)
     void move_auto_tilt(){
         arm_pub_tilt.publish(angle_tilt);
-        if(angle_tilt.data>=1.57)  tilt_sim =1;
-        if(angle_tilt.data<=-1.57) tilt_sim=0;
+        if(angle_tilt.data>=1.569)  tilt_sim =1;
+        if(angle_tilt.data<=-1.569) tilt_sim=0;
 
         if(tilt_sim==1) angle_tilt.data=angle_tilt.data-0.2;
         else if (tilt_sim==0) angle_tilt.data=angle_tilt.data+0.2;
@@ -106,14 +103,12 @@ private:
     ros::Subscriber sub;
     ros::Publisher real_arm_single;
     ros::Publisher real_arm_group;
-    ros::Publisher send_velo;
     ros::Publisher real_arm_traj;
     ros::NodeHandle n;
     pckg::JointSingleCommand msg_single;
     pckg::JointGroupCommand msg_group;
     int pan =1;
     int tilt=1;
-    sensor_msgs::JointState jointstate;
     trajectory_msgs::JointTrajectory traj;
     pckg::JointTrajectoryCommand msg_traj;
     //trajectory_msgs::JointTrajectoryPoint points_n;
@@ -124,12 +119,10 @@ public:
         sub = n.subscribe("wxxms/joint_states", 1000, &pan_tilt_real::get_position_arm, this);
         real_arm_single= n.advertise<pckg::JointSingleCommand>("/wxxms/commands/joint_single", 1000);
         real_arm_group= n.advertise<pckg::JointGroupCommand>("/wxxms/commands/joint_group", 1000);
-        send_velo= n.advertise<sensor_msgs::JointState>("/wxxms/joint_states", 1000);
         real_arm_traj=n.advertise<pckg::JointTrajectoryCommand>("/wxxms/commands/joint_trajectory",1000);
     }
     //function to display the data of the robot you want
     void get_position_arm(const sensor_msgs::JointState& state){
-        //ROS_INFO("RECEIVED JOINT VALUES");
         if (state.name.size()== 2 ) {
            for (int i=0;i<2;i++){
             ROS_INFO_STREAM("\n RECEIVED JOINT VALUES :"
@@ -173,8 +166,6 @@ public:
     void move_auto_real_robot(){
         msg_group.name="all";
         msg_group.cmd.resize(2);
-        //msg_group.cmd[1]=0.0;
-        //msg_group.cmd[0]=0.0;
         if (msg_group.cmd[0]>=3.1388) pan=1;
         else if(msg_group.cmd[0]<= -3.1388) pan=0;
 
@@ -192,34 +183,39 @@ public:
     void traject(){
         msg_traj.cmd_type="group";
         msg_traj.name="all";
+        msg_traj.traj.header.frame_id="pan_tilt";
+
         msg_traj.traj.header.stamp=ros::Time::now();
-        msg_traj.traj.header.frame_id="";
+
         msg_traj.traj.joint_names.resize(2);
         msg_traj.traj.joint_names[0]="pan";
         msg_traj.traj.joint_names[1]="tilt";
 
+        //set the trajectory
         msg_traj.traj.points.resize(8);
         msg_traj.traj.points[0].positions.resize(2);
         msg_traj.traj.points[0].positions[0]=-3.0;
         msg_traj.traj.points[0].positions[1]=-1.5;
-        msg_traj.traj.points[0].time_from_start = ros::Duration(1);
-        //real_arm_traj.publish(msg_traj);
-        double j=-3.0;
+        msg_traj.traj.points[0].time_from_start = ros::Duration(6);
 
+        double j=-3.0;
         for(double i=1;i<8;i++){
 
             msg_traj.traj.points[i].positions.resize(2);
             msg_traj.traj.points[i].positions[0]=j*1.04666667;
             msg_traj.traj.points[i].positions[1]=(1.04666*j)/2;
+            /*msg_traj.traj.points[i].velocities.resize(2);
+            msg_traj.traj.points[i].velocities[0]=-0.2;
+            msg_traj.traj.points[i].velocities[1]=0.2;*/
             j++;
-            msg_traj.traj.points[i].time_from_start =  msg_traj.traj.points[i-1].time_from_start+ros::Duration(6.0);
-            real_arm_traj.publish(msg_traj);
+            msg_traj.traj.points[i].time_from_start = msg_traj.traj.points[i-1].time_from_start+ros::Duration(6.0);
         }
+        real_arm_traj.publish(msg_traj);
     }
-
 };
 
 int main (int argc, char** argv){
+    //Initialize the ROS system and become a node
     ros::init(argc, argv, "pan_tilt");
     //pan_tilt_simu robot_simu;
     pan_tilt_real robot_real;
@@ -227,13 +223,13 @@ int main (int argc, char** argv){
 
     while(ros::ok()){
         //robot_simu.init_angle();
-        //robot_simu.set_angle_pan_tilt(10.0, -11.0);
+        //robot_simu.set_angle_pan_tilt(3.0, 1.0);
         //robot_simu.move_auto_pan();
         //robot_simu.move_auto_tilt();
         //robot_simu.random_position();
         //robot_real.move_real_robot_single("tilt", 0.0);
         //robot_real.move_real_robot_single("pan", 1.0);
-        //robot_real.move_manu_real_robot_group(-1.0, -1.0);
+        //robot_real.move_manu_real_robot_group(0.0, 0.0);
         //robot_real.move_auto_real_robot();
         robot_real.traject();
         ros::spinOnce();
